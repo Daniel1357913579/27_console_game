5d7a6b185d3c6bf908c1fd76217796c59618dc03
Ethan <ethanl202@nycstudents.net>
2024-12-12 09:25:13 -0500

    dasdjef

--- Driver.java
+++ Driver.java
@@ -1,5 +1,12 @@
 public class Driver{
 public static void main(String[] args){
+// Magic: Ninja,
+// Support: Banana Farm,
+// Military: sniper
+Adventurer ninja1 = new Ninja();
+Adventurer farm1 = new Farm();
+Adventurer snipper1 = new Sniper();
+
 
 }
 
