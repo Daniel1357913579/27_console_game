e557703271d952d0412c3fc691c36541366a728d
Ethan <ethanl202@nycstudents.net>
2024-12-13 09:25:48 -0500

    rawr

--- Adventurer.java
+++ Adventurer.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 public abstract class Adventurer{
+
   private String name;
   private int HP,maxHP;
   /*There is no no-arg constructor. Be careful with your subclass constructors.*/
--- Driver.java
+++ Driver.java
@@ -1,13 +1,43 @@
 public class Driver{
 public static void main(String[] args){
-// Magic: Ninja,
-// Support: Banana Farm,
-// Military: sniper
-Adventurer ninja1 = new Ninja();
-Adventurer farm1 = new Farm();
-Adventurer snipper1 = new Sniper();
+//addventurar
+//
+///
+Adventurer fighter1 = new Fighter("Bob",1000);
+Adventurer grenadier1 = new Grenadier("farmer",10000);
+Adventurer snipper1 = new Sniper("snipper",100);
+
+p(fighter1.getName());
+p(fighter1.restoreSpecial());
+p(grenadier1.restoreSpecial());
+p(fighter1.getSpecialName());
+p(fighter1.setSpecial(3));
+p(fighter1.getSpecialMax());
+p(grenadier.attack(fighter1));
+p(fighter1.support(grenadier));
+p(grenadier.getHP());
+figher1.support();
+fighter1.getHP();
+figther1.setName();
+p(fighter1.getName);
+
+
+
+
+
 
 // give 2nd and 4th tier upgrades
 }
-
+public static void p(string k){
+  System.out.println(k);
+}
+public static void p(int k){
+  System.out.println(k);
+}
+public static void p(double k){
+  System.out.println(k);
+}
+public static void p(Boolean k){
+  System.out.println(k);
+}
 }
